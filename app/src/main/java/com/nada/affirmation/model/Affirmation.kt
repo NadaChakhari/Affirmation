07dbf6f131e8359d1e312b733cfb29e0121f1b23
NadaChakhari <nadachakhari00@gmail.com>
2021-11-17 11:41:06 +0100

    commit_1

--- app/src/main/java/com/nada/affirmation/model/Affirmation.kt
+++ app/src/main/java/com/nada/affirmation/model/Affirmation.kt
@@ -1,3 +1,9 @@
 package com.nada.affirmation.model
 
-class Affirmation (val stringResourceId: Int)
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+class Affirmation (@StringRes val stringResourceId: Int,
+                   @DrawableRes val imageResourceId: Int){
+
+}
